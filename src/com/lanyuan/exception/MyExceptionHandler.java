3de5a717e4bf86d8211d68704c6e3c8bbca610f7
jwang <308306362@qq.com>
2014-11-23 20:23:11 +0800

    git test

---
+++ src/com/lanyuan/exception/MyExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.lanyuan.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+public class MyExceptionHandler implements HandlerExceptionResolver{
+
+	@Override
+	public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handel, Exception e) {
+		Map<String,Object> model=new HashMap<String,Object>();
+		model.put("exception", e);
+		return new ModelAndView("error", model);
+	}
+
+}
